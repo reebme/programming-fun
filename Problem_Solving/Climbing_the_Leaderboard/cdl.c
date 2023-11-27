7d5716e68afd9786cb04dbc1427d8c4d92677f45
Magdalena Ruducha <magdalena.ruducha@gmail.com>
2023-11-27 14:48:48 +0100

    handling input

--- Problem_Solving/Climbing_the_Leaderboard/cdl.c
+++ Problem_Solving/Climbing_the_Leaderboard/cdl.c
@@ -4,10 +4,29 @@
 
 int main(){
    
-    //the # of players
+    // the # of players
     char *input_line = get_line();
     int n = read_int(input_line); 
     free(input_line);
+    // players' scores
+    input_line = get_line();
+    int *high_scores = (int *)malloc(n * sizeof(int));
+    read_n_ints(n, input_line, high_scores);
+    free(input_line);
+    // # of games
+    input_line = get_line();
+    int no_games = read_int(input_line);
+    free(input_line);
+    // scores
+    input_line = get_line();
+    int *game_scores = (int *)malloc(no_games * sizeof(int));
+    read_n_ints(no_games, input_line, game_scores);
+    free(input_line);
+
+    printf("No of players: %d\n", n);
+    print_array(n, (int *)high_scores);
+    printf("No of games: %d\n", no_games);
+    print_array(no_games, game_scores);
 
     exit(EXIT_SUCCESS);
 }
