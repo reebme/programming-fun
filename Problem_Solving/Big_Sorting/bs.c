e620c8afa8618f544f4829d1c3cf2f5629a678a4
Magdalena Ruducha <magdalena.ruducha@gmail.com>
2023-11-30 12:20:27 +0100

    added an array of lengths of strings in integers

--- Problem_Solving/Big_Sorting/bs.c
+++ Problem_Solving/Big_Sorting/bs.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <utils.h>
 
 int main(void){
+    char* input = get_line();
+    // number of elements to be sorted
+    int no_elem = read_int(input);
+    free(input);
+    printf("\n# of elements to be sorted: %d\n", no_elem);
 
+    char *integers[no_elem];
+    int len[no_elem];
+    for(int i = 0; i < no_elem; ++i){
+        integers[i] = get_line();
+        len[i] = strlen(integers[i]) - 1; //-1 to account for \n
+        //printf("%s: %d\n", integers[i], len[i]);
+    }
 }
