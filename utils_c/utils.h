49798f137341c49aea46146c898e2ce566eae823
Magdalena Ruducha <magdalena.ruducha@gmail.com>
2023-12-01 09:50:33 +0100

    added a fuction searching for max element in an array

--- utils_c/utils.h
+++ utils_c/utils.h
@@ -5,6 +5,7 @@ int read_n_ints(int, char *, int *);
 void print_matrix(int, int, int **);
 void print_array(int, int *);
 int min_array(int n, int *arr);
+int max_array(int n, int *arr);
 int *generate_array(int n, int min_val, int max_val);
 int index_of(int *elem, int n, int *arr);
 int *select_distinct_ints(int *, int, int *);
