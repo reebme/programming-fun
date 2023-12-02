75476495507329291e47df0c597c747dfe5a3fc3
Magdalena Ruducha <magdalena.ruducha@gmail.com>
2023-12-02 15:14:03 +0100

    added measuring the time it takes to read input and perform a radix sort

--- Problem_Solving/Big_Sorting/bs.c
+++ Problem_Solving/Big_Sorting/bs.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <utils.h>
 #include <sort_utils.h>
 
@@ -11,13 +12,25 @@ int main(void){
     free(input);
     printf("\n# of elements to be sorted: %d\n", no_elem);
 
+    clock_t start, end;
+    double time_elapsed;
+    
     char *integers[no_elem];
     int len[no_elem];
+    start = clock();
     for(int i = 0; i < no_elem; ++i){
         integers[i] = get_line();
         len[i] = strlen(integers[i]) - 1; //-1 to account for \n
         //printf("%s: %d\n", integers[i], len[i]);
     }
+    end = clock();
+    time_elapsed = ((double)(end - start))/CLOCKS_PER_SEC;
+    printf("Time elapsed on reading input: %d seconds\n", time_elapsed);
+    
     int max_int_len = len[max_array((int *)len, no_elem)];
+    start = clock();
     radix_sort((char **)integers, (int *) len, no_elem, max_int_len);
+    end = clock();
+    time_elapsed = ((double)(end - start))/CLOCKS_PER_SEC;
+    printf("Time elapsed on radix sort: %d seconds\n", time_elapsed);
 }
