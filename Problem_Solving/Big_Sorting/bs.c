693ee739da8622f6230542855f5b5aac67a52ebf
Magdalena Ruducha <magdalena.ruducha@gmail.com>
2023-12-03 17:08:52 +0100

    calculated maximum length of the number represented by string and fixed radix_sort call

--- Problem_Solving/Big_Sorting/bs.c
+++ Problem_Solving/Big_Sorting/bs.c
@@ -15,21 +15,21 @@ int main(void){
     clock_t start, end;
     double time_elapsed;
     
-    char *integers[no_elem];
-    int len[no_elem];
+    big_int integers[no_elem];
+    size_t max_int_len = 0;
     start = clock();
     for(int i = 0; i < no_elem; ++i){
-        integers[i] = get_line();
-        len[i] = strlen(integers[i]) - 1; //-1 to account for \n
-        //printf("%s: %d\n", integers[i], len[i]);
+        integers[i].num = get_line();
+        integers[i].len = rtrim(integers[i].num); // trim \n at the end
+        if(max_int_len < integers[i].len)
+            max_int_len = integers[i].len;
     }
     end = clock();
     time_elapsed = ((double)(end - start))/CLOCKS_PER_SEC;
     printf("Time elapsed on reading input: %d seconds\n", time_elapsed);
     
-    int max_int_len = len[max_array((int *)len, no_elem)];
     start = clock();
-    radix_sort((char **)integers, (int *) len, no_elem, max_int_len);
+    radix_sort((big_int *)integers, no_elem, max_int_len);
     end = clock();
     time_elapsed = ((double)(end - start))/CLOCKS_PER_SEC;
     printf("Time elapsed on radix sort: %d seconds\n", time_elapsed);
